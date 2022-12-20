90dbd1345dd5ae6097838892b34111dd5f8452a8
habiba mohsen <118688798+HabibaMohsen@users.noreply.github.com>
2022-12-20 23:30:05 +0200

    update2

---
+++ react-axios-skeleton-master/src/components/bookpage/scientific/scientific.jsx
@@ -0,0 +1,54 @@
+const scientific=[
+
+    {
+        id: "",
+        image: "",
+        BookName: "",
+        BookAuthor: "",
+        Price: "",
+      },
+    
+      {
+        id: "",
+        image: "",
+        BookName: "",
+        BookAuthor: "",
+        Price: "",
+      },
+    
+      {
+        id: "",
+        image: "",
+        BookName: "",
+        BookAuthor: "",
+        Price: "",
+      },
+    
+      {
+        id: "",
+        image: "",
+        BookName: "",
+        BookAuthor: "",
+        Price: "",
+      },
+    
+      {
+        id: "",
+        image: "",
+        BookName: "",
+        BookAuthor: "",
+        Price: "",
+      },
+    
+      {
+        id: "",
+        image: "",
+        BookName: "",
+        BookAuthor: "",
+        Price: "",
+      },
+
+
+
+
+]
