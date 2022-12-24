e78d1af0b1dde45e347e9af240cdd60c067bc329
habiba mohsen <118688798+HabibaMohsen@users.noreply.github.com>
2022-12-25 01:52:48 +0200

    up3

---
+++ react-axios-skeleton-master/src/components/bookpage/historical.jsx
@@ -0,0 +1,53 @@
+const historicalList=[
+
+    {
+        id: "1",
+        image: "images/historical/img1.jpg",
+        BookName: "البلاط الملكي",
+        BookAuthor: "عبود مصطفى",
+        Price: "٦٣٫٠٠ ج.م",
+      },
+    
+      {
+        id: "2",
+        image: "images/historical/img2.jpg",
+        BookName:"النقود العربية وعلم النميات",
+        BookAuthor: "أنستاس الكرملي",
+        Price: "٤١٠٫٠٠ ج.م",
+      },
+    
+      {
+        id: "3",
+        image: "images/historical/img3.jpg",
+        BookName:" الابله",
+        BookAuthor:"دوستويفسكي",
+        Price: "٦٨٫٠٠ ج.م",
+      },
+    
+      {
+        id: "4",
+        image: "images/historical/img4.jpg",
+        BookName: "مقتطفات من تاريخ العالم",
+        BookAuthor: "جمال الكاشف",
+        Price: "٤٥٫٠٠ ج.م",
+      },
+    
+      {
+        id: "5",
+        image: "images/historical/img5.jpg",
+        BookName: "الحرب السيبرانية",
+        BookAuthor: "منصور عبد الحكيم",
+        Price: "١٠٧٫٠٠ ج.م",
+      },
+    
+      {
+        id: "6",
+        image: "images/historical/img6.jpg",
+        BookName: "للتاريخ أقوال أخرى",
+        BookAuthor: "أحمد المنزلاوى",
+        Price: "٩٤٫٠٠ ج.م",
+      },
+
+
+]
+export default historicalList;
---
+++ react-axios-skeleton-master/src/components/bookpage/historicalPage.jsx
@@ -0,0 +1,41 @@
+import { react } from "react";
+import historicalList from './historical'
+import BookCard from "./BookCard";
+import {Container, Grid} from "@material-ui/core";
+import useStyles from "./Style"
+
+const Historical = () => {
+const classes=useStyles();
+  return (
+
+    <div>
+      <Container  className={classes.cardGrid} maxWidth="md">
+     <Grid  padding={1} spacing={3} container >
+        {historicalList.map((historical) => (
+          <Grid xs={6}
+          sm={4}
+          md={3}
+          lg={4}
+           height={'500px'}
+             width={"100%"}
+            
+              item 
+               Key={historical.id}>
+            <BookCard
+             id={historical.id}
+            image={historical.image}
+            BookAuthor={historical.BookAuthor}
+            BookName={historical.BookName}
+            Price={historical.Price}
+            />
+          </Grid>
+        ))}
+        
+     </Grid>
+     </Container>
+     </div>
+    
+  );
+};
+
+export default Historical;
