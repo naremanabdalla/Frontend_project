6e7a3861327fa43e240ffa846547ed0b1f6ea34a
narema abdalla <120642444+naremanabdalla@users.noreply.github.com>
2022-12-26 01:18:02 +0200

    clc4

--- react-axios-skeleton-master/src/constants/routes.js
+++ react-axios-skeleton-master/src/constants/routes.js
@@ -1,17 +1,17 @@
 export const LANDING = '/';
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
+export const homepage_map = '/homepage_map';
 export const BookCard = '/BookCard';
 export const CardsPage = '/CardsPage';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const About_us = '/About_us';
+export const Contact_us = '/Contact_us';
 export const Scientific = '/scientific';
 export const Historical = '/historical';
 export const Languages = '/languages';
-export const page2 ='/auth/About_Contact_pages/page2/page2';
-export const page3 ='/auth/About_Contact_pages/page3/page3';
-export const page6 ='/auth/About_Contact_pages/page6/page6';
-export const Contact_us ='/auth/About_Contact_pages/Contact_us/Contact_us';
-export const About_us ='/auth/About_Contact_pages/About_us/About_us';
-
+export const page2 = '/page2';
+export const page3 = '/page3';
+export const page6 = '/page6';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const ADMIN = '/admin';
