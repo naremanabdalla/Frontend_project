f5a7212932822e08e7575c40f45809bcbf577122
narema abdalla <120642444+naremanabdalla@users.noreply.github.com>
2022-12-26 00:18:29 +0200

    clac2

--- react-axios-skeleton-master/src/components/auth/About_Contact_pages/page3/page3.jsx
+++ react-axios-skeleton-master/src/components/auth/About_Contact_pages/page3/page3.jsx
@@ -1,6 +1,8 @@
 import { makeStyles } from '@material-ui/styles';
 import date from './page3_date';
-import {AppBar,MenuItem,Toolbar, Container,Card,CardMedia,CardContent,CardActions,Button,Typography} from "@material-ui/core";
+import Link from "@material-ui/core/Link";
+import * as ROUTES from '../../../../constants/routes';
+import { AppBar, MenuItem, Toolbar, Container, Card, CardMedia, CardContent, CardActions, Button, Typography } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
 const useStyles = makeStyles({
     Card: { height: "80%", display: "flex", flexDirection: "column", padding: "25px 25px" }
@@ -10,41 +12,53 @@ const useStyles = makeStyles({
 });
 const page3 = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const classes = useStyles ();
+    const classes = useStyles();
 
-  return (
-    <>
-     <AppBar position="static">
+    return (
+        <>
+            <AppBar position="static">
                 <Toolbar>
-                <b className='title'>الروايات</b>
-                <Toolbar/>  
-                <MenuItem >كتب علم النفس  </MenuItem>
-                <MenuItem >كتب الأدب </MenuItem>
-                <MenuItem >Contact_us</MenuItem>
-                <MenuItem >About_us</MenuItem>    
+                    <b className='title'>Psychology</b>
+                    <Toolbar />
+                    <Link href={ROUTES.page6} variant="body2">
+                        <Button >Novels</Button>
+                    </Link>
+                    <Link href={ROUTES.page2} >
+                        <Button >Literature</Button>
+                    </Link>
+                    <Link href={ROUTES.Historical} >
+                        <Button >Historical Book</Button>
+                    </Link>
+                    <Link href={ROUTES.Scientific} >
+                        <Button >Scientific Book</Button>
+                    </Link>
+                    <Link href={ROUTES.Languages} >
+                        <Button >Languages</Button>
+                    </Link>
                 </Toolbar>
             </AppBar>
-< Container className={classes.cardGrid} maxWidth="md"> 
-    <Grid container spacing={4}>
-        {date.map((page3_date) => (                       
+            < Container className={classes.cardGrid} maxWidth="md">
+                <Grid container spacing={4}>
+                    {date.map((page3_date) => (
                         <Grid item key={page3_date.id}>
-                                <Card className={classes.Card}>
-                                    <CardMedia className={classes.CardMedia}
-                                        image={page3_date.image} title="book"></CardMedia>
-                                    <CardContent className={classes.CardContent}>
-                                        <b><p>{page3_date.title}</p></b>
-                                        <Typography>{page3_date.Another}</Typography>
-                                        <Typography>{page3_date.price}</Typography>
-                                        <CardActions className={classes.CardActions}>
-                                            <Button size='small' color='primary' variant="contained">Add To Card</Button>
-                                        </CardActions>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                             ))}
-                              </Grid>
-                             </Container>    </>
-  );
+                            <Card className={classes.Card}>
+                                <CardMedia className={classes.CardMedia}
+                                    image={page3_date.image} title="book"></CardMedia>
+                                <CardContent className={classes.CardContent}>
+                                    <b><p>{page3_date.title}</p></b>
+                                    <Typography>{page3_date.Another}</Typography>
+                                    <Typography>{page3_date.price}</Typography>
+                                    <CardActions className={classes.CardActions}>
+                                        <Button size='small' color='primary' variant="contained">Add To Card</Button>
+                                    </CardActions>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </>
+    );
 };
 
 export default page3;
--- react-axios-skeleton-master/src/components/home_page/homepage_array.jsx
+++ react-axios-skeleton-master/src/components/home_page/homepage_array.jsx
@@ -3,31 +3,37 @@ const homepage_array = [
         id: "1",
         cardName: "Literature Book",
         image: "img_homepage/book_Lit1.jpg",
+        links: "page2"
     },
     {
         id: "2",
         cardName: "Historical Book",
         image: "img_homepage/book_His1.jpg",
+        links: "Historical"
     },
     {
         id: "3",
         cardName: "Scientific Book",
         image: "img_homepage/book_sci1.jpg",
+        links: "Scientific"
     },
     {
         id: "4",
         cardName: "Psychology",
         image: "img_homepage/book_Psy1.jpg",
+        links: "page3"
     },
     {
         id: "5",
         cardName: "Novels",
         image: "img_homepage/book_Nov1.jpg",
+        links: "page6"
     },
     {
         id: "6",
         cardName: "Languages",
-        image: "img_homepage/book_Lang1.jpg"
+        image: "img_homepage/book_Lang1.jpg",
+        links: "Languages"
     },
 ]
 
