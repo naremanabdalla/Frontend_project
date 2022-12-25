ae2cf1910c8ed956fc1f8362489b04f74d905c33
Mai Mahmoud Mohmed <120642469+Mai565@users.noreply.github.com>
2022-12-25 23:48:47 +0200

    ..

--- react-axios-skeleton-master/src/components/auth/About_Contact_pages/Contact_us/Contact_us.jsx
+++ react-axios-skeleton-master/src/components/auth/About_Contact_pages/Contact_us/Contact_us.jsx
@@ -1,23 +1,36 @@
+import { makeStyles } from "@material-ui/core/styles";
 import * as React from 'react';
-import { Button, TextField } from '@material-ui/core';
-import "./Contact_us.css"
+import {Container, Button, TextField } from '@material-ui/core';
+const useStyles = makeStyles(() => ({
+    paper: {
+      alignItems: "center"
+    }, 
+       form: {
+        width: "50%"
+        ,
+        alignItems: "center" 
+      },
+    }));
 const Contact_us = () => {
+    const classes = useStyles();
 
     return (
 
         <>
-            <h2 className='contact' >
+< Container className={classes.cardGrid}>
+            <h2 className='contact' align="center" >
                 <b> Contact_us </b>
             </h2>
-            <TextField className='t1' id="first_name" label="first_name" />
-            <TextField className='t2' id="Last_name" label="Last_name" />
-            <TextField className='t3' id="the topic" label="the topic" />
-            <TextField className='t4' id=" Yours Email" label=" Yours Email" />
-            <TextField className='t5' id=" Leave us a message " label=" Leave us a message " />
+            <TextField  className={classes.form} id="first_name" label="first_name" />
+            <TextField className={classes.form} id="Last_name" label="Last_name" />
+            <TextField className={classes.form} id="the topic" label="the topic" />
+            <TextField className={classes.form} id=" Yours Email" label=" Yours Email" />
+            <TextField className={classes.form} id=" Leave us a message " label=" Leave us a message " />
             <Button className="Button1" variant="contained"  >
                 send
             </Button>
 
+            </Container>
         </>
 
     )
