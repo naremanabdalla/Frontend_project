f79c1c67359849f65655c7db3a74bb08fef670aa
Mai Mahmoud Mohmed <120642469+Mai565@users.noreply.github.com>
2022-12-25 21:54:26 +0200

    .

--- react-axios-skeleton-master/src/constants/routes.js
+++ react-axios-skeleton-master/src/constants/routes.js
@@ -6,6 +6,11 @@ export const CardsPage = '/CardsPage';
 export const Scientific = '/scientific';
 export const Historical = '/historical';
 export const Languages = '/languages';
+export const page2 ='/auth/About_Contact_pages/page2/page2';
+export const page3 ='/auth/About_Contact_pages/page3/page2=3';
+export const page6 ='/auth/About_Contact_pages/page6/page2=6';
+export const Contact_us ='/auth/About_Contact_pages/Contact_us/Contact_us';
+export const About_us ='/auth/About_Contact_pages/About_us/About_us';
 
 export const HOME = '/home';
 export const ACCOUNT = '/account';
