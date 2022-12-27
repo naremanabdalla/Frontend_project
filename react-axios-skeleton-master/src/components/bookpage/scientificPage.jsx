5cfc9667ad4b25028d9cffde34686e94113673c6
habiba mohsen <118688798+HabibaMohsen@users.noreply.github.com>
2022-12-27 23:20:32 +0200

    update.

--- react-axios-skeleton-master/src/components/bookpage/scientificPage.jsx
+++ react-axios-skeleton-master/src/components/bookpage/scientificPage.jsx
@@ -12,29 +12,24 @@ const Scientific = () => {
 
   return (
     <div >    
-      <AppBar position="static">
-<Toolbar>
-  <b className="title">BookSky</b>
-
-<Link href={ROUTES.page3} variant="body2">
-  <Button>Novels</Button>
-</Link>
-<Link href={ROUTES.page2} variant="body2">
-  <Button>Literature</Button>
-</Link>
-<Link href={ROUTES.page6} variant="body2">
-  <Button>Psychology</Button>
-</Link>
-<Link href={ROUTES.Historical} variant="body2">
-  <Button>Historical</Button>
-</Link>
-<Link href={ROUTES.Languages} variant="body2">
-  <Button>Languages</Button>
-</Link>
-<Link href={ROUTES.Scientific} variant="body2">
-  <Button>Science</Button>
-</Link>
-</Toolbar>
+     <AppBar position="static">
+        <Toolbar>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }} style={{ flex: 1 }}>
+            BookSky store
+          </Typography>
+          <Link href={ROUTES.Contact_us} variant="body2">
+            <Button variant="text" color="success" >contactus</Button>
+          </Link>
+          <Link href={ROUTES.About_us} variant="body2">
+            <Button variant="text" color="success" >Aboutus</Button>
+          </Link>
+          <Link href={ROUTES.SIGN_IN} variant="body2">
+            <Button variant="text" color="success">Signin</Button>
+          </Link>
+          <Link href={ROUTES.SIGN_UP} variant="body2">
+            <Button variant="text" color="success">register</Button>
+          </Link>
+        </Toolbar>
       </AppBar>
 <Container className={classes.cardGrid}  maxWidth="md" >
  
@@ -61,7 +56,53 @@ const Scientific = () => {
         
      </Grid>
      </Container>
-     </div>
+     <div style={{
+        background: "#848482",
+        padding: "20px",
+        margin: "5px",
+      }}>
+        <Container maxWidth="9g" style={{ marginTop: "10px" }}>
+          <Typography variant='h6'
+            align="left"
+            color='textPrimary'
+            gutterBottom>
+            <table>
+              <thead>
+                <tr>
+                  <td>instractions</td>
+                  <td>Company</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><Link href={ROUTES.Contact_us} variant="body2" color='inherit'>Terms of service</Link></td>
+                  <td><Link href={ROUTES.Contact_us} variant="body2" color='inherit'>contact us</Link></td>
+                </tr>
+                <tr>
+                  <td><Link href={ROUTES.About_us} variant="body2" color='inherit'>Copyright </Link></td>
+                  <td><Link href={ROUTES.About_us} variant="body2" color='inherit'>About us</Link></td>
+                </tr>
+                <tr>
+                  <td><Link href={ROUTES.About_us} variant="body2" color='inherit'>Return policy</Link></td>
+                </tr>
+              </tbody>
+            </table>
+          </Typography>
+          <div style={{ padding: "20px", margin: "5px" }}>
+            <Container maxWidth="9g" >
+              <Typography variant='h7'
+                align="center"
+                color='textPrimary'
+                gutterBottom>
+                All rights reserved to BookSky 2021-2022 - by our teams
+              </Typography>
+            </Container>
+          </div>
+        </Container>
+      </div>
+    </div>
+
+     
     
   );
 };
