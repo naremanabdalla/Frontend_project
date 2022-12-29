4275fc61e877dfc33773f3e132a97684aafc295d
Mai Mahmoud Mohmed <120642469+Mai565@users.noreply.github.com>
2022-12-29 10:05:51 +0200

    .

--- react-axios-skeleton-master/src/components/About_Contact_pages/About_us/About_us.jsx
+++ react-axios-skeleton-master/src/components/About_Contact_pages/About_us/About_us.jsx
@@ -4,10 +4,10 @@ const About_us = () => {
     return (
 
         <>
-            <h2 className='About' >
-                <b className='title'>About_us</b>
+            <h2 className='About' align="center" >
+                <b className='title'  >About_us</b>
             </h2>
-            <p className='p1'>
+            <p className='p1' align="center">
                 <p> We are students at the Faculty of Science, Cairo University, a single computer department
                 </p><p>This is a strategic location for selling various books with a bright view of hopes and aspirations
                 </p><p>This project is very good and the students did:</p>
